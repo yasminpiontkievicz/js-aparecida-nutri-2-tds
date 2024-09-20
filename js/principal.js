f8beab6d540ce3fad20fe6361138cf152a5abd75
Yasmin Eduarda Piontkievicz <160152483+yasminpiontkievicz@users.noreply.github.com>
2024-09-20 12:26:49 -0300

    Kickoff-nutricionista

---
+++ js/principal.js
@@ -0,0 +1,88 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+var tdImc = tdImc.textContent;
+var imc = peso / altura * altura;
+
+console.log(imc);
+console.log(tdImc);
+console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
+/////////
+var paciente = document.querySelector("#segundo-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+var tdImc = tdImc.textContent;
+var imc = peso / altura * altura;
+console.log(imc);
+console.log(tdImc);
+console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
+////////////////
+var paciente = document.querySelector("#terceiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+var tdImc = tdImc.textContent;
+var imc = peso / altura * altura;
+console.log(imc);
+console.log(tdImc);
+console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
+////////////////
+var paciente = document.querySelector("#quarto-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+var tdImc = tdImc.textContent;
+var imc = peso / altura * altura;
+console.log(imc);
+console.log(tdImc);
+console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
+////////////////
+var paciente = document.querySelector("#quinto-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+var tdImc = tdImc.textContent;
+var imc = peso / altura * altura;
+console.log(imc);
+console.log(tdImc);
+console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
